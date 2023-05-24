ee6890d71224e99c6529afb23616dc3888a14cbb
dylanlikelihood <95553931+dylanlikelihood@users.noreply.github.com>
2023-05-24 15:38:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 #! /usr/bin/env python3
+"""
+    Title: main.py
+    Author: Dylan Armbruster
+    Data: 5/24/2023
+"""
 
 import data_utils
 
@@ -10,37 +15,8 @@ def main():
     print("Saddleback Registration")
     studentID = data_utils.login()
 
-        # Display menu
-    while True:
-        data_utils.displayMenu()
-        selection = input("Enter selection: ")
-        if selection.lower() == "info":
-            print("You're in info")
-            # Student information
-        elif selection.lower() == "list":
-            print("You're in list")
-            # Course listing
-        elif selection.lower() == "detail":
-            print("You're in detail")
-            # Course detail
-        elif selection.lower() == "register":
-            print("You're in register")
-            # Register for a class
-        elif selection.lower() == "drop":
-            print("You're in drop")
-            # Drop class
-        elif selection.lower() == "menu":
-            print("You're in menu")
-            # Menu
-        else: 
-            print("Invalid selection. Please try again.\n")
-    
-
 
     
-            
-
-
 
 if __name__ == "__main__":
     main()
