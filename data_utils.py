481f9a4ac3c18b892514eefb760bff37aa8dd650
dylanlikelihood <95553931+dylanlikelihood@users.noreply.github.com>
2023-05-23 19:00:35 -0700

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -3,7 +3,6 @@
 import csv
 import os
 
-
 FILENAME = "students.csv"
 
 student_data = []
@@ -22,17 +21,15 @@ def readStudentFile(file_name):
         print(f"File '{my_file}' not found.")
         exit()
 
+# Add a check to see if the file opened
+readStudentFile(FILENAME)
+
 # Student Validation Function
 def validStudent(student_id):
     for student in student_data:
         if student_id == student[0]:
             return True
     return False
-
-# Rest of the code...
-
-# Add a check to see if the file opened
-readStudentFile(FILENAME)
 
 # Display menu
 def displayMenu():
@@ -48,57 +45,71 @@ def displayMenu():
 
 # Add New Student Function
 def addNewStudent(studentID):
-    if studentID == "add":
-      students = ""
-      last_name = input("Enter the student's last name: ").lower()
-      first_name = input("Enter the student's first name: ").lower()
+    # Variables
+    first_name = ""
+    last_name = ""
+
+    while True:
+        # Get student's last name
+        last_name = input("Enter the student's last name: ")
+        # Input validation
+        if last_name.isalpha() and last_name.strip() != "":
+            break
+        else:
+            print("Student's last name can only contain alphabetic letters and cannot be left blank.")
+      
+        # Get student's first name
+        first_name = input("Enter the student's first name: ")
+        # Input validation
+        if first_name.isalpha() and first_name.strip() != "":
+            break
+        else:
+            print("Student's first name can only contain alphabetic letters and cannot be left blank.")
 
     # Generate student ID
-    numeric_value = 0
-    student_id = f"{first_name[0]}{last_name}{numeric_value}"
-    while validStudent(student_id):
-        numeric_value += 1
-        student_id = f"{first_name[0]}{last_name}{numeric_value}"
+    student_id = f"{first_name[0].lower()}{last_name.lower()}"
+    index = 0
+     
+    while f'{student_id}{index}' in [student[0] for student in student_data]:
+        index += 1
+    
+    student_id = f"{student_id}{index}"
+    student_data.append([student_id, last_name, first_name])
+
     print(f"Student {student_id} has been added.\n")
     print(f"Good morning {first_name}, what would you like to do today?\n")
 
-      
+    # Display menu
     while True:
-          displayMenu()
-          selection = input("Enter selection: ")
-          if selection.lower() == "info":
+        displayMenu()
+        selection = input("Enter selection: ")
+        if selection.lower() == "info":
             print("You're in info")
-            # student information
-          elif selection.lower() == "list":
-            print("you're in list")
-            # course listing
-          elif selection.lower() == "detail":
-             print("You're in detail")
-             # Course detail
-          elif selection.lower() == "register":
-            print("You'r'e in register")
-            # register for a class
-          elif selection.lower() == "drop":
+            # Student information
+        elif selection.lower() == "list":
+            print("You're in list")
+            # Course listing
+        elif selection.lower() == "detail":
+            print("You're in detail")
+            # Course detail
+        elif selection.lower() == "register":
+            print("You're in register")
+            # Register for a class
+        elif selection.lower() == "drop":
             print("You're in drop")
-            # drop class
-          elif selection.lower() == "menu":
-             print("You're in menu")
-             # menu
-          else: 
-            print("Exit")
-            break
+            # Drop class
+        elif selection.lower() == "menu":
+            print("You're in menu")
+            # Menu
+        else: 
+            print("Invalid selection. Please try again.\n")
 
 
 # Login Function
 def login():
     while True:
         studentID = input("\nEnter Student Id (or 'add' to add a new student, or 'exit' to exit the application): ")
+        addNewStudent(studentID)
 
         if studentID == 'exit':
             print("\nSession ended.\n")
-            exit()
-
-        if validStudent(studentID):
-            break
-        addNewStudent(studentID)
-        return studentID
